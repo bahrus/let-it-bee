c74aac23ced642178b6fd8c0f95461873e0df745
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-29 14:39:45 -0400

    Initial commit

---
+++ types.d.ts
@@ -0,0 +1,16 @@
+import { EndUserProps as Active} from 'be-active/types';
+import { EndUserProps as Definitive } from 'be-definitive/types';
+import { EndUserProps as Ferried} from 'be-ferried/types';
+import { INotifyMap } from 'be-noticed/types'; 
+import { EndUserProps as Switched } from 'be-switched/types';
+import { EndUserProps as Loaded } from 'be-loaded/types';
+
+import { EndUserProps as Exportable} from 'be-exportable/types';
+import { EndUserProps as Consensual } from 'be-consensual/types';
+import { EndUserProps as LookingUp } from 'be-looking-up/types';
+import { EndUserProps as Reformable } from 'be-reformable/types';
+import { EndUserProps as Importing } from 'be-importing/types';
+import { EndUserProps as Based } from 'be-based/types';
+import { EndUserProps as Repeated } from 'be-repeated/types';
+import { IChannel } from 'be-channeling/types';
+export {Definitive as BeDefinitive}
